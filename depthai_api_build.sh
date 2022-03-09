719b95b33c2c5b078e546958536034ee144857ec
xiao tang <2310296215@qq.com>
2022-03-09 09:03:51 +0800

    Annotate web launch commands

--- depthai_api_build.sh
+++ depthai_api_build.sh
@@ -4,4 +4,4 @@ cmake -H. -Bbuild -D DEPTHAI_BUILD_DOCS=ON -D DEPTHAI_PYTHON_BUILD_DOCS=ON
 
 cmake --build build --target sphinx
 
-python3 -m http.server --bind 0.0.0.0 8000 --directory build/docs/sphinx
+# python3 -m http.server --bind 0.0.0.0 8000 --directory build/docs/sphinx
